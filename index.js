870fcf8dc2276d8be519dbaf37758f3bb617f35f
WrongTeck <wrongteck@protonmail.com>
2021-06-27 14:49:56 +0200

    Load modules for WS Improved the start of the WS Improved index.js

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-const config=require('./config/main.json');
-
-const discord=require('discord.js');
-const client=new discord.Client();
-
-const fs=require('fs');
-
-const { MongoClient }=require('mongodb');
-
-const clientMongo=new MongoClient(config.mongodb.connection_str);
-async function mongo(){
+const main = require('./config/main.json');
+const discord = require('discord.js');
+const client = new discord.Client();
+const fs = require('fs');
+const { MongoClient } = require('mongodb');
+const clientMongo = new MongoClient(main.mongodb.connection_str);
+async function mongo() {
     await clientMongo.connect();
-    let db=clientMongo.db(config.mongodb.name);
+    let db = clientMongo.db(main.mongodb.name);
     return db;
 }
-if(config.websocket.ssl.use){
-    let start=require('./websocket/useSSL.js');
-    start.execute(client);
-}else{
-    let ol=require('./websocket/noSSL.js');
-    ol.execute(client);
-}
-//keep this at the bottom of this file
-client.login(config.discord.token); //Login in to the discord API
-fs.readdir('./plugins',(err,files)=>{
-    //Load all plugins from the folder
-    if(err) return console.error(err) && process.exit(1);
-    let db=mongo();
-    files.forEach((f,i,array)=>{
-        const element=require(`./plugins/${f[i]}/index.js`);
-        element.main(client,db);
+client.on('ready', () => {
+    fs.readdir('./plugins', (err, files) => {
+        //Load all plugins from the folder
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        let db = mongo();
+        files.forEach((f, i, array) => {
+            const element = require(`./plugins/${f[i]}/index.js`);
+            element.main(client, db);
+        });
     });
+    if (main.websocket.ssl.use) {
+        let start = require('./websocket/useSSL.js');
+        start.execute(client, mongo());
+    }
+    else {
+        let ol = require('./websocket/noSSL.js');
+        ol.execute(client, mongo());
+    }
+    console.log('Ready! ' + client.ws.ping + "ms");
 });
+//keep this at the bottom of this file
+client.login(main.discord.token); //Login in to the discord API
--- websocket/noSSL.js
+++ websocket/noSSL.js
@@ -1,7 +1,9 @@
 const config = require('../config/main.json');
 const ws = require('ws');
-const wss = new ws.Server({ port: config.websocket.port, path: "/admin" });
-function execute(client) {
+const usage = require('usage');
+const fs = require('fs');
+function execute(client, db) {
+    const wss = new ws.Server({ port: config.websocket.port, path: "/admin" });
     wss.on('connection', (connection, req) => {
         connection.on('ping', (data) => {
             connection.pong(data);
@@ -13,12 +15,25 @@ function execute(client) {
                     process.exit(0);
                     break;
                 case "status":
-                    connection.send(`CPU:${process.cpuUsage()}\nRAM:${process.memoryUsage()}\nPing:${client.ws.ping}`);
+                    usage.lookup(process.pid, (err, result) => {
+                        connection.send(`CPU: ${Math.trunc(result.cpu)}%\nRAM: ${Math.floor(result.memory / (1024 * 1024))}MB\nPing: ${client.ws.ping}ms`);
+                    });
                     break;
                 default:
                     connection.send("I don't know what is this! Use help to view the help");
                     break;
             }
+        });
+        fs.readdir('./plugins', (err, files) => {
+            //Load all plugins from the folder
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+            files.forEach((f, i, array) => {
+                const element = require(`./plugins/${f[i]}/index.js`);
+                element.main(wss, client, db);
+            });
         });
     });
 }
