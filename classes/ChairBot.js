1bef3293d19c7a016e4021e49faff44539a10825
WrongTeck <wrongteck@protonmail.com>
2021-07-10 14:58:18 +0200

    Now plugins need to require what the need example: discord,expres,ws etc

--- classes/ChairBot.js
+++ classes/ChairBot.js
@@ -1,86 +1,131 @@
-const discord=require('discord.js');
-const client=new discord.Client();
-const express=require('express');
-const app=express();
-const {Plugins}=require('./Plugins');
-const ws = require('ws');
-const usage = require('usage');
-const fs=require('fs');
-class ChairBot{
-    constructor(cb){
-        this.config=readconfig();
-        this.discordload(this.config.discord,client=>{
-            this.discord=client;
-            this.express=this.expressload(this.config.express);
-            this.ws=this.websocket(this.config.websocket);
+const { Plugins } = require("./Plugins");
+const fs = require("fs");
+const ioredis = require("ioredis");
+class ChairBot {
+    constructor(cb) {
+        this.config={};
+        /*this.mongo(this.config.mongodb);
+            this.discordload(this.config.discord,client=>{
+                this.discord=client;
+                this.express=this.expressload(this.config.express);
+                this.ws=this.websocket(this.config.websocket);
+                this.moduleloader();
+                this.pluginloader(plugins=>{
+                    this.plugins=plugins;
+                    cb(this);
+                });
+            });*/
             this.moduleloader();
-            this.pluginloader(plugins=>{
-                this.plugins=plugins;
+            this.pluginloader(()=>{
                 cb(this);
             });
-        });
     }
-    discordload(config,cb){ //Pass the discord config only
-        if(config && config.enable){
+    discordload() {
+        const config = require_ifexist("discord");
+        if (config) {
+            this.config.discord=config;
+            const discord = require("discord.js");
+            const client = new discord.Client();
             client.login(config.token);
-            client.on('ready',()=>{
-                cb(client);
+            client.on("ready", () => {
+                console.log(`[INFO] Discord is ready! Ping ${client.ws.ping}ms`);
             });
-        }else{
-            cb("");
+            this.discord = client;
         }
     }
-    expressload(config){
-        if(config && config.enable){
-            app.listen(config.port,()=>{
-                console.log('Listening on '+config.port);
+    expressload() {
+        const express = require("express");
+        const app = express();
+        const config = require_ifexist("express");
+        if (config) {
+            this.config.express=config;
+            app.listen(config.port, () => {
+                console.log(`[INFO] Express listening on ${config.port}`);
             });
-            return {
-                express:express,
-                app:app
-            };
+            this.express = express;
+            this.express.app = app;
         }
     }
-    websocket(config,express){
-        if(config && config.enable){
-            if(!express) throw new Error('Could not start the WebSocket without express!');
-            const wss = new ws.Server({ port: config.port, path: config.path, server:express });
-            wss.on('connection', (connection, req) => {
-                connection.on('ping', (data) => {
+    websocket() {
+        const config = require_ifexist("websocket");
+        if (config) {
+            this.config.ws=config;
+            const ws = require("ws");
+            const usage = require("usage");
+            if (!this.express)
+                throw new Error("Could not start the WebSocket without express!");
+            const wss = new ws.Server({
+                port: config.port,
+                path: config.path,
+                server: express,
+            });
+            wss.on("connection", (connection, req) => {
+                connection.on("ping", (data) => {
                     connection.pong(data);
                 });
-                connection.on('message', (data) => {
+                connection.on("message", (data) => {
                     switch (data) {
                         case "shutdown":
-                            connection.send('The bot will shutdown in few seconds');
+                            connection.send("The bot will shutdown in few seconds");
                             process.exit(0);
                             break;
                         case "status":
                             usage.lookup(process.pid, (err, result) => {
-                                connection.send(`CPU: ${Math.trunc(result.cpu)}%\nRAM: ${Math.floor(result.memory / (1024 * 1024))}MB\nPing: ${client.ws.ping}ms`);
+                                connection.send(
+                                    `CPU: ${Math.trunc(result.cpu)}%\nRAM: ${Math.floor(result.memory / (1024 * 1024))}MB\nPing: ${client.ws.ping}ms`
+                                );
                             });
                             break;
                         default:
-                            connection.send("I don't know what is this! Use help to view the help");
+                            connection.send(
+                                "I don't know what is this! Use help to view the help"
+                            );
                             break;
                     }
                 });
             });
-            return wss;
+            this.ws = wss;
         }
     }
-    pluginloader(cb){
-        new Plugins(this,(plugins)=>{
-            cb(plugins);
+    mongo_con() {
+        const config = require_ifexist("mongodb");
+        if (config) {
+            this.config.mongo=config;
+            const {
+                MongoClient
+            } = require("mongodb");
+            const mongo = new MongoClient(config.uri);
+            this.mongo = mongo;
+        }
+    }
+    redis_con() {
+        if (config) {
+            this.config.redis=config;
+            const redis = new ioredis(config.port, config.host);
+            if (config.username && config.passwrd) {
+                redis.auth(config.username, config.password, (err, res) => {
+                    if (err)
+                        throw new Error("Unable to authenticate in the rdeis server");
+                    this.redis = res;
+                    return;
+                });
+            }
+            this.redis = redis;
+        }
+    }
+    pluginloader(cb) {
+        new Plugins(this, (plugins) => {
+            this.plugins = plugins;
+            cb();
         });
     }
-    moduleloader(){
-        const files=fs.readdirSync('./modules');
-        this.modules={};
+    moduleloader() {
+        const files = fs.readdirSync("./modules");
+        this.modules = {};
         try {
             for (const key in files) {
-                if(!files[key].includes('.')){
-                    let t=require(`../modules/${files[key]}/index.js`);
+                if (!files[key].includes(".")) {
+                    let t = require(`../modules/${files[key]}/index.js`);
                     t.main(this);
                 }
             }
@@ -89,26 +134,33 @@ class ChairBot{
         }
     }
 }
-/* 
-* Config Loader 
-*/
-    function require_ifexist(name){
-        return fs.readFileSync(`./config/${name}.json`);
-    }
-    function readconfig(){
-        var files=fs.readdirSync('./config');
-        var config="";
-        for(let key in files) {
-                if(files[key].endsWith('.json')){
-                let c=files[key].replace('.json','');
-                let t=require_ifexist(c);
-                if(!config){
-                    config=`"${c}":${t}`;
-                }else{
-                    config=config+`,"${c}":"${t}"`;
-                }
+/*
+ * Config Loader
+ */
+async function require_ifexist(name) {
+    /*return fs.readFileSync(`./config/${name}.json`);*/
+    await fs.readFile(`./config/${name}.json`, (e, data) => {
+        if (e) return "";
+        return data;
+    });
+}
+
+function readconfig() {
+    var files = fs.readdirSync("./config");
+    var config = "";
+    for (let key in files) {
+        if (files[key].endsWith(".json")) {
+            let c = files[key].replace(".json", "");
+            let t = require_ifexist(c);
+            if (!config) {
+                config = `"${c}":${t}`;
+            } else {
+                config = config + `,"${c}":"${t}"`;
             }
         }
-        return JSON.parse(`{${config}}`);
     }
-module.exports={ChairBot};
+    return JSON.parse(`{${config}}`);
+}
+module.exports = {
+    ChairBot
+};
--- classes/Plugins.js
+++ classes/Plugins.js
@@ -3,7 +3,8 @@ const fs = require("fs");
 class Plugins{
     constructor(chairbot,cb){
         if(chairbot){
-            this.loader(chairbot,(loaded)=>{
+            this.chairbot=chairbot;
+            this.loader(chairbot,()=>{
                 cb(this.plugins);
             });
         }
@@ -11,26 +12,58 @@ class Plugins{
     loader(chairbot,cb){
         fs.readdir('./plugins',{encoding:'utf-8'},(err,files)=>{
             if(err) throw new Error('Could not read the plugins directory!\nError:\n'+err);
-            var loaded={};
+            this.plugins={};
             for (const key in files) {
                 if(!files[key].toString().includes('.')){
                     try{
                         const {Plugin}=require(`../plugins/${files[key]}/index.js`);
                         let pl=new Plugin(chairbot);
-                        pl.main(chairbot,loaded);
+                        pl.main(chairbot);
                         console.log(`Loaded ${files[key]}`);
                     }catch(e){
                         console.error(`Could not load ${files[key]}:\n${e}`);
                     }
-                    if(!this.plugins){
-                        this.plugins=loaded;
-                    }else{
-                        this.plugins=this.plugins+loaded;
-                    }
                 }
             }
-            cb(this.plugins);
+            cb();
         });
+    }
+    require(name){
+        switch(name){
+            case "discord":
+                if(!this.discord){
+                    this.chairbot.discordload();
+                    this.discord=true;
+                }
+                break;
+            case "express":
+                if(!this.express){
+                    this.chairbot.expressload();
+                    this.express=true;
+                }   
+                break;
+            case "ws":
+                if(!this.ws){
+                    this.chairbot.websocket();
+                    this.ws=true;
+                }
+                break;
+            case "mongo":
+                if(!this.mongo){
+                    this.chairbot.mongo_con();
+                    this.mongo=true;
+                }
+                break;
+            case "redis":
+                if(!this.redis){
+                    this.chairbot.redis_con();
+                    this.redis=true;
+                }
+                break;
+            default:
+                console.log(`[WARN] A plugin tryed to require ${name} but is not a valid name`);
+                break;
+            }
     }
 }
 module.exports={Plugins};
