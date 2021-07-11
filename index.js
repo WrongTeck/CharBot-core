bc44122a49ee006abb457a9040c4a0d25e2043f5
WrongTeck <wrongteck@protonmail.com>
2021-07-11 17:39:31 +0200

    Fixed some bugs in the discord loader

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ class ChairBot {
         this.modulesloader();
         this.pluginsloader();
     }
-    async configloader(name) {
+    configloader(name) {
         const fs = require('fs');
         async function run(mongo,config){
             try {
@@ -18,45 +18,34 @@ class ChairBot {
                 mongo.close();
             }
         }
-        fs.readFile('./config/main.json', {
-            encoding: 'utf-8'
-        }, (err, data) => {
-            if (err) this.logger('err',`main.json is REQUIRED to start the bot!\n Error: ${err}`);
-            const main = JSON.parse(data);
+        const data=fs.readFileSync('./config/main.json');
+        const main = JSON.parse(data);
             if (main.mongo.enable && name !== "mongo") {
                 this.mongoloader();
                 return run(this.mongo,this.config.mongo);
             } else {
-                fs.readFile(`./config/${name}.json`, {
-                    encoding: 'utf-8'
-                }, (err, data) => {
-                    if (err) {
-                        this.logger('err',`Could not load ${name}.json from config folder!`);
-                        return "";
-                    }
-                    return JSON.parse(data);
-                });
+                    const file=fs.readFileSync(`./config/${name}.json`);
+                    return JSON.parse(file);
             }
-        });
     }
-    async discordloader(cb) {
-        const config = await this.configloader('discord');
-        if (config.token && !this.discord) {
-            const {
-                Client
-            } = require('discord.js');
-            const client = new Client();
+    discordloader() {
+        const config=this.configloader('discord');
+        if (config && !this.discord) {
+            const discord = require('discord.js');
+            this.logger('err',"ddd");
+            const client = new discord.Client();
+            client.login(config.token);
             client.on('ready', () => {
                 this.logger('info',`Discord is ready! Ping ${client.ws.ping}ms`);
             });
-            return client;
+            this.discord=client;
         } else {
             this.logger('err','Invalid discord config!');
             return 'err';
         }
     }
-    async expressloader() {
-        const config = await this.configloader('express');
+    expressloader() {
+        const config = this.configloader('express');
         if (config && !this.express) {
             this.config.express=config;
             const express = require("express");
@@ -70,8 +59,8 @@ class ChairBot {
             return 'err';
         }
     }
-    async websocket() {
-        const config = await this.configloader('websocket');
+    websocket() {
+        const config = this.configloader('websocket');
         if (config && !this.ws) {
             this.config.ws=config;
             const ws = require('ws');
@@ -84,8 +73,8 @@ class ChairBot {
             return 'err';
         }
     }
-    async mongoloader() {
-        const config = await this.configloader('mongo');
+    mongoloader() {
+        const config = this.configloader('mongo');
         if (config.uri && !this.mongo) {
             this.config.mongo=config;
             const {
@@ -96,8 +85,8 @@ class ChairBot {
             return "err";
         }
     }
-    async redisloader() {
-        const config = await this.configloader('redis');
+    redisloader() {
+        const config = this.configloader('redis');
         if (config.port && !this.redis) {
             this.config.redis=config;
             const ioredis = require('ioredis');
@@ -109,7 +98,7 @@ class ChairBot {
     }
     logger(type, message) {
         type = type.toUpperCase();
-        console.log(new Date().toUTCString() + `${type} ${message}`);
+        console.log(new Date().toUTCString() + ` [${type}] ${message}`);
     }
     pluginsloader(){
         const fs=require('fs');
@@ -118,9 +107,8 @@ class ChairBot {
             for (const key in files) {
                 try {
                     const {Plugin}=require(`./plugins/${files[key]}/index.js`);
-                    new Plugin(this,(main)=>{
-                        main();
-                    });
+                    const p=new Plugin(this);
+                    p.main(this);
                 } catch (error) {
                     this.logger('err',`Could not load ${files[key]}\nError: ${error}`);
                 }
@@ -142,3 +130,4 @@ class ChairBot {
         });
     }
 }
+new ChairBot();
