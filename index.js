2066b1235bb20d867f8d590948c9579c0784d943
WrongTeck <wrongteck@protonmail.com>
2021-07-13 15:04:29 +0200

    Created TS files

--- index.js
+++ index.js
@@ -1,120 +1,125 @@
 class ChairBot {
     constructor() {
-        this.config={};
-        this.modules={};
-        this.plugins={};
+        this.config = {};
+        this.modules = {};
+        this.plugins = {};
         this.modulesloader();
         this.pluginsloader();
     }
     configloader(name) {
         const fs = require('fs');
-        async function run(mongo,config){
+        async function run(mongo, config) {
             try {
                 await mongo.connect();
-                let db=mongo.db(config.mongo.db);
-                let res=await db.findOne({type:'config',config:`${name}`});
+                let db = mongo.db(config.mongo.db);
+                let res = await db.findOne({ type: 'config', config: `${name}` });
                 return res;
-            } finally {
+            }
+            finally {
                 mongo.close();
             }
         }
-        const data=fs.readFileSync('./config/main.json');
+        const data = fs.readFileSync('./config/main.json');
         const main = JSON.parse(data);
-            if (main.mongo.enable && name !== "mongo") {
-                this.mongoloader();
-                return run(this.mongo,this.config.mongo);
-            } else {
-                    const file=fs.readFileSync(`./config/${name}.json`);
-                    return JSON.parse(file);
-            }
+        if (main.mongo.enable && name !== "mongo") {
+            this.mongoloader();
+            return run(this.mongo, this.config.mongo);
+        }
+        else {
+            const file = fs.readFileSync(`./config/${name}.json`);
+            return JSON.parse(file);
+        }
     }
     discordloader() {
-        const config=this.configloader('discord');
+        const config = this.configloader('discord');
         if (config && !this.discord) {
             const discord = require('discord.js');
-            this.logger('err',"ddd");
+            this.logger('err', "ddd");
             const client = new discord.Client();
             client.login(config.token);
             client.on('ready', () => {
-                this.logger('info',`Discord is ready! Ping ${client.ws.ping}ms`);
+                this.logger('info', `Discord is ready! Ping ${client.ws.ping}ms`);
             });
-            if(config.debug){
-                client.on('debug',debug=>{
-                    this.logger('debug',debug);
+            if (config.debug) {
+                client.on('debug', debug => {
+                    this.logger('debug', debug);
                 });
             }
-            this.discord=client;
-        } else {
-            this.logger('err','Invalid discord config!');
+            this.discord = client;
+        }
+        else {
+            this.logger('err', 'Invalid discord config!');
             return 'err';
         }
     }
     expressloader() {
         const config = this.configloader('express');
         if (config && !this.express) {
-            this.config.express=config;
+            this.config.express = config;
             const express = require("express");
             const app = express();
             app.listen(config.port, () => {
-                this.logger('info',`Express is listening on ${config.port}`);
+                this.logger('info', `Express is listening on ${config.port}`);
             });
             this.express = express;
             this.app = app;
-        } else {
+        }
+        else {
             return 'err';
         }
     }
     websocket() {
         const config = this.configloader('websocket');
         if (config && !this.ws) {
-            this.config.ws=config;
+            this.config.ws = config;
             const ws = require('ws');
             const wss = new ws.Server({
                 path: config.path,
                 server: this.app,
             });
-            this.ws=wss;
-            this.logger('info',`WebSocket ready!`);
-        } else {
-            this.ws= 'err';
+            this.ws = wss;
+            this.logger('info', `WebSocket ready!`);
+        }
+        else {
+            this.ws = 'err';
         }
     }
     mongoloader() {
         const config = this.configloader('mongo');
         if (config.uri && !this.mongo) {
-            this.config.mongo=config;
-            const {
-                MongoClient
-            } = require('mongodb');
+            this.config.mongo = config;
+            const { MongoClient } = require('mongodb');
             this.mongo = new MongoClient(config.uri, config.options);
-            this.logger('info',`MongoDB connector ready!`);
-        } else {
-            this.mongo= "err";
+            this.logger('info', `MongoDB connector ready!`);
+        }
+        else {
+            this.mongo = "err";
         }
     }
     redisloader() {
         const config = this.configloader('redis');
         if (config.port && !this.redis) {
-            this.config.redis=config;
+            this.config.redis = config;
             const ioredis = require('ioredis');
             const redis = new ioredis(config.port, config.host, config.options);
             this.redis = redis;
-            this.logger('info','Redis connector ready!');
-        } else {
-            this.redis= "err";
+            this.logger('info', 'Redis connector ready!');
+        }
+        else {
+            this.redis = "err";
         }
     }
     logger(type, message) {
-        const chalk=require('chalk');
-        const moment=require('moment');
+        const chalk = require('chalk');
+        const moment = require('moment');
         const fs = require('fs');
         type = type.toUpperCase();
-        if(!this.log){
-            this.log=moment().format(`HH-mm-ss`)+'-chairbot';
+        if (!this.log) {
+            this.log = moment().format(`HH-mm-ss`) + '-chairbot';
         }
-        const time=moment().format('HH:mm:ss');
-        switch(type){
-            case 'ERR':
+        const time = moment().format('HH:mm:ss');
+        switch (type) {
+            case 'ERR2':
                 console.log(chalk.red(`${time} [${type}] ${message}`));
                 break;
             case 'INFO':
@@ -147,62 +152,66 @@ class ChairBot {
             default:
                 console.log(chalk.red(`[${time}] [Unknown] ${message}`));
         }
-        if(type=="LOGGER"){
+        if (type == "LOGGER") {
             process.exit(1);
-        }else if(type=='FATAL'){
-           log(this.log,time,type,message,()=>{
-               process.exit(1);
+        }
+        else if (type == 'FATAL') {
+            log(this.log, time, type, message, () => {
+                process.exit(1);
             });
         }
-        function log(log,time,type,message,cb){
-            fs.appendFile(`./logs/${log}.log`,`[${time}] [${type}] ${message}\n`,(err)=>{
-                if(err){
-                    fs.mkdir('./logs',(err=>{
-                        if(err){
-                            if(cb){
+        function log(log, time, type, message, cb) {
+            fs.appendFile(`./logs/${log}.log`, `[${time}] [${type}] ${message}\n`, (err) => {
+                if (err) {
+                    fs.mkdir('./logs', (err => {
+                        if (err) {
+                            if (cb) {
                                 cb();
                             }
-                            return logger('logger','Unable to write logs!'+err);
                         }
-                        if(cb){
+                        if (cb) {
                             cb();
                         }
                     }));
                 }
-                if(cb){
+                if (cb) {
                     cb();
                 }
             });
         }
-        log(this.log,time,type,message);
+        log(this.log, time, type, message, () => { });
     }
-    pluginsloader(){
-        const fs=require('fs');
-        fs.readdir('./plugins',(err,files)=>{
-            if(err) return this.logger('fatal',`Could not read the plugins folder! \n ${err}`);
+    pluginsloader() {
+        const fs = require('fs');
+        fs.readdir('./plugins', (err, files) => {
+            if (err)
+                return this.logger('fatal', `Could not read the plugins folder! \n ${err}`);
             for (const key in files) {
                 try {
-                    const {Plugin}=require(`./plugins/${files[key]}/index.js`);
-                    const p=new Plugin(this);
+                    const { Plugin } = require(`./plugins/${files[key]}/index.js`);
+                    const p = new Plugin(this);
                     p.main(this);
-                    this.logger('PL',`Loaded ${p.name}`);
-                } catch (error) {
-                    this.logger('err',`Could not load ${files[key]}\n ${error}`);
+                    this.logger('PL', `Loaded ${p.name}`);
+                }
+                catch (error) {
+                    this.logger('err', `Could not load ${files[key]}\n ${error}`);
                 }
             }
         });
     }
-    modulesloader(){
+    modulesloader() {
         const fs = require('fs');
-        fs.readdir('./modules',(err,files)=>{
-            if(err) return this.logger('fatal',`Could not read the modules folder! \n ${err}`);
+        fs.readdir('./modules', (err, files) => {
+            if (err)
+                return this.logger('fatal', `Could not read the modules folder! \n ${err}`);
             for (const key in files) {
                 try {
-                    const {Module}=require(`./modules/${files[key]}/index.js`);
-                    const m=new Module(this);
-                    this.logger('ML',`Loaded ${m.name}`);
-                } catch (error) {
-                    this.logger('err',`Could not load ${files[key]}\n ${error}`);
+                    const { Module } = require(`./modules/${files[key]}/index.js`);
+                    const m = new Module(this);
+                    this.logger('ML', `Loaded ${m.name}`);
+                }
+                catch (error) {
+                    this.logger('err', `Could not load ${files[key]}\n ${error}`);
                 }
             }
         });
