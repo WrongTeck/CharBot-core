7102100b6ea050992427721ad523a561f9cfdf2f
WrongTeck <me@wrongteck.xyz>
2021-12-26 17:10:47 +0100

    Moved the project to TypeScript

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-const Console = require("./classes/Console");
-const CharBot = require("./classes/CharBot");
-const Logger = require("./classes/Logger");
-const PlaceHolders = require("./classes/PlaceHolders");
-const ModuleLoader = require("./classes/ModuleLoader");
-const PluginLoader = require("./classes/PluginLoader");
-
-
+import CharBot from "./classes/CharBot";
+import CharConsole from "./classes/Console";
+import Logger from "./classes/Logger";
+import PlaceHolders from "./classes/PlaceHolders";
+import ModuleLoader from "./classes/ModuleLoader";
+import PluginLoader from "./classes/PluginLoader";
 try {
-  const bot = new CharBot();
-  bot.on("loadingConfig", () => {
-    this.bot.console.log("Loading config...");
-  });
-} catch (error) {
-  console.error(error);
+    var bot = new CharBot();
 }
-
-module.exports = {
-  CharBot,
-  Logger,
-  Console,
-  PlaceHolders,
-  ModuleLoader,
-  PluginLoader,
-};
+catch (error) {
+    console.error(error);
+}
+export { CharBot, CharConsole, PlaceHolders, Logger, PluginLoader, ModuleLoader };
